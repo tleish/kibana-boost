6a69a0f42b8dec219cd7df8643bd64b0e9fb1758
tleish <tleish@users.noreply.github.com>
2024-07-24 23:18:23 -0600

    Fix @updateURL and @downloadURL

--- dist/kibana_boost.user.js
+++ dist/kibana_boost.user.js
@@ -2,8 +2,8 @@
 // @name         Kibana Boost
 // @namespace    https://github.com/tleish/
 // @version      0.1
-// updateURL     https://github.com/tleish/kibana-boost/raw/main/dist/kibana_boost.meta.js
-// downloadURL   https://github.com/tleish/kibana-boost/raw/main/dist/kibana_boost.user.js
+// @updateURL     https://github.com/tleish/kibana-boost/raw/main/dist/kibana_boost.meta.js
+// @downloadURL   https://github.com/tleish/kibana-boost/raw/main/dist/kibana_boost.user.js
 // @description  Updates Kibana view
 // @match        http://127.0.0.1:9200/_plugin/kibana/app/kibana*
 // @copyright    2024+, tleish
