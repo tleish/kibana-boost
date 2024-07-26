4f18b3ae61d58b4bfb660d870fb38b9cf0c32a0c
tleish <tleish@users.noreply.github.com>
2024-07-25 18:35:10 -0600

    fix CSV download

--- src/lib/XHRListener.js
+++ src/lib/XHRListener.js
@@ -4,39 +4,28 @@ export default class XHRListener {
     this.shouldListen = shouldListen;
     this.init();
   }
-
   init() {
     const originalOpen = XMLHttpRequest.prototype.open;
     const originalSend = XMLHttpRequest.prototype.send;
-    const originalSetRequestHeader = XMLHttpRequest.prototype.setRequestHeader;
     const listener = this;
 
     XMLHttpRequest.prototype.open = function(method, url, ...rest) {
       this._shouldListen = listener.shouldListen(url);
-      this._method = method;
-      this._url = url;
-      this._requestHeaders = {};
       return originalOpen.apply(this, [method, url, ...rest]);
     };
 
-    XMLHttpRequest.prototype.setRequestHeader = function(header, value) {
+    XMLHttpRequest.prototype.send = function(...args) {
       if (this._shouldListen) {
-        this._requestHeaders[header] = value;
+        this.addEventListener('load', function() {
+          try {
+            const data = JSON.parse(this.responseText).responses[0].hits.hits.map(hit => hit._source);
+            listener.dataStorage.storeData(JSON.stringify(data));
+          } catch (error) {
+            console.error('Error handling XHR response:', error);
+          }
+        });
       }
-      return originalSetRequestHeader.apply(this, arguments);
-    };
-
-    XMLHttpRequest.prototype.send = function(body) {
-      if (this._shouldListen) {
-        const requestData = {
-          method: this._method,
-          url: this._url,
-          headers: this._requestHeaders,
-          body: body
-        };
-        listener.dataStorage.storeData(JSON.stringify(requestData));
-      }
-      return originalSend.apply(this, arguments);
+      return originalSend.apply(this, args);
     };
   }
 }
